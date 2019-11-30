a91f22e3b89fa342a282cbf40904c17e3aa61d05
isanbel <theisanbel@gmail.com>
2019-12-01 00:40:16 +0800

    Start developing with devui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { DevUIModule } from 'ng-devui';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DevUIModule
   ],
   providers: [],
   bootstrap: [AppComponent]
