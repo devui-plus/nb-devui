041d74eb7d6b18a2117917666f02e7a220219629
isanbel <theisanbel@gmail.com>
2019-12-01 08:26:11 +0800

    Create components page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ComponentsComponent } from './components/components.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  {
+    path: 'components/get-start', component: ComponentsComponent
+  },
+  {
+    path: 'components/:component/demo', component: ComponentsComponent
+  },
+  { path: '', redirectTo: '/components/get-start', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
 
