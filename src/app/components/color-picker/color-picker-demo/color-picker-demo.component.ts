8bfd87bed7b940e83d3592e49db343f52b4effbd
isanbel <theisanbel@gmail.com>
2019-12-06 17:13:37 +0800

    feat: basic color picker

--- src/app/components/color-picker/color-picker-demo/color-picker-demo.component.ts
+++ src/app/components/color-picker/color-picker-demo/color-picker-demo.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./color-picker-demo.component.scss']
 })
 export class ColorPickerDemoComponent implements OnInit {
-
+  recentlySelected = ["rgba(69,226,250,0.9)", "rgba(33,37,232,0.5)", "rgba(2,139,216,0.8)"]
   constructor() { }
 
   ngOnInit() {
--- src/app/components/color-picker/color-picker.component.ts
+++ src/app/components/color-picker/color-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-color-picker',
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./color-picker.component.scss']
 })
 export class ColorPickerComponent implements OnInit {
+  @Input() recentlySelected: string[];
+  // @Output() select: new EventEmitter();
+
+  selected = "rgba(255,255,255,1)"
+  preColor = 'rgba(255,255,255,1)';
+  recentColor = 'rgba(255,255,255,1)';
 
   constructor() { }
 
   ngOnInit() {
+    this.initColors()
+    this.checkRecentlySelected()
   }
 
+  initColors() {
+    if (this.recentlySelected.length) {
+      this.preColor = this.recentlySelected[0]
+      this.preColor = this.recentlySelected[0]
+    }
+  }
+
+  getRandomRgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+  }
+
+  checkRecentlySelected() {
+    this.recentlySelected = this.recentlySelected.slice(0, 10)
+  }
+
+  colorPanelSelect() {
+    this.selected = this.getRandomRgba()
+    this.recentColor = this.selected
+  }
+
+  save() {
+    this.preColor = this.recentColor
+    this.recentlySelected.unshift(this.selected)
+    this.checkRecentlySelected()
+    console.log(this.selected)
+  }
 }
