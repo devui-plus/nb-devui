67fbfbb47008b13cdc5979e4edcca92e75695043
kagol <kagol@sina.com>
2020-01-17 00:01:02 +0800

    feat: 增加Mithril版本的ColorInput组件

--- projects/mr-devui-plus/src/lib/mr-devui-plus.component.ts
+++ projects/mr-devui-plus/src/lib/mr-devui-plus.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as m from 'mithril';
-import { ColorCube } from '../components/color-picker/color-cube';
+import { ColorInput } from '../components/color-picker/color-input';
 
 @Component({
   selector: 'm-mr-devui-plus',
@@ -16,7 +16,7 @@ export class MrDevuiPlusComponent implements OnInit {
 
   ngOnInit() {
     const root = document.querySelector('#mithril-root');
-    m.render(root, m(ColorCube, { color: '#1daf68' }))
+    m.mount(root, ColorInput);
   }
 
 }
