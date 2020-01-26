a38a42b09bb8580fad7e8117299a044edae3cf7d
isanbel <theisanbel@gmail.com>
2020-01-26 10:29:29 +0800

    fix: remove AdvancedColorComponent from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AdvancedColorComponent } from './advanced-color/advanced-color.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PageNotFoundComponent,
-    AdvancedColorComponent
   ],
   imports: [
     BrowserModule,
